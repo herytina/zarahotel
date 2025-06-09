6dd2f715041f4773e3699789908b0b7fc9ae36d8
NastinaRijalalaina <rijaandriamaherisoa88@gmail.com>
2025-06-09 15:35:25 +0300

    feat create path eventRoom, apartment and ecoResponsibility

--- src/router.js
+++ src/router.js
@@ -5,56 +5,92 @@ import Landing from './pages/Landing.vue';
 import Profile from './pages/Profile.vue';
 import StarterPage from './pages/StarterPage.vue';
 import indexPage from './pages/indexPage.vue';
+import Aparment from './pages/Aparment.vue';
+import EventRoom from './pages/EventRoom.vue';
+import EcoResponsibility from './pages/EcoResponsibility.vue';
 
-
-
-  const routes = [
-    {
-      path: '/',
-      name: 'indexPage',
-      components: { default: indexPage, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 200 },
-        footer: { backgroundColor: 'black' }
-      }
+const routes = [
+  {
+    path: '/',
+    name: 'indexPage',
+    components: { default: indexPage, header: MainNavbar, footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 200 },
+      footer: { backgroundColor: 'black' },
     },
-    {
-      path: '/hotel',
-      name: 'hotel',
-      components: { default: indexPage, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 200 },
-        footer: { backgroundColor: 'black' }
-      }
+  },
+  {
+    path: '/hotel',
+    name: 'hotel',
+    components: { default: indexPage, header: MainNavbar, footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 200 },
+      footer: { backgroundColor: 'black' },
     },
-    {
-      path: '/chambre',
-      name: 'chambre',
-      components: { default: Landing, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 200 },
-        footer: { backgroundColor: 'black' }
-      }
+  },
+  {
+    path: '/chambre',
+    name: 'chambre',
+    components: { default: Landing, header: MainNavbar, footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 200 },
+      footer: { backgroundColor: 'black' },
     },
-    {
-      path: '/service',
-      name: 'service',
-      components: { default: Profile, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 200 },
-        footer: { backgroundColor: 'black' }
-      }
+  },
+  {
+    path: '/service',
+    name: 'service',
+    components: { default: Profile, header: MainNavbar, footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 200 },
+      footer: { backgroundColor: 'black' },
     },
-    {
-      path: '/contact',
-      name: 'contact',
-      components: { default: StarterPage, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 200 },
-        footer: { backgroundColor: 'black' }
-      }
-    }
-  ]
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    components: {
+      default: StarterPage,
+      header: MainNavbar,
+      footer: MainFooter,
+    },
+    props: {
+      header: { colorOnScroll: 200 },
+      footer: { backgroundColor: 'black' },
+    },
+  },
+  {
+    path: '/apartment',
+    name: 'ApartmentPage',
+    components: { default: Aparment, header: MainNavbar, footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 200 },
+      footer: { backgroundColor: 'black' },
+    },
+  },
+  {
+    path: '/eventRoom',
+    name: 'EventRoomPage',
+    components: { default: EventRoom, header: MainNavbar, footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 200 },
+      footer: { backgroundColor: 'black' },
+    },
+  },
+  {
+    path: '/ecoResponsibility',
+    name: 'EcoResponsibilityPage',
+    components: {
+      default: EcoResponsibility,
+      header: MainNavbar,
+      footer: MainFooter,
+    },
+    props: {
+      header: { colorOnScroll: 200 },
+      footer: { backgroundColor: 'black' },
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
