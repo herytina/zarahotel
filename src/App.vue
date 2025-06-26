d02967804f2e6e36bda8201eab5a041a04d05d9d
NastinaRijalalaina <rijaandriamaherisoa88@gmail.com>
2025-06-26 22:33:45 +0300

    fix implement Lato font

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
-    <router-view name="header" />
-    <div class="wrapper">
+    <router-view name="header" class="header" />
+    <div class="wrapper body">
       <router-view />
     </div>
-    <router-view name="footer" />
+    <router-view name="footer" class="footer" />
   </div>
 </template>
 <script>
 export default {};
 </script>
+
+<style scoped>
+.header {
+  font-family: Lato;
+}
+
+.body {
+  font-family: Lato;
+}
+
+.footer {
+  font-family: Lato;
+}
+</style>
